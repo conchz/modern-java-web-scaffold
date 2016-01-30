d8a94b79f4db34727922dcfb370b2a4ab367612f
dolphineor <dolphineor@gmail.com>
2016-01-30 23:54:34 +0800

    [master] 前端集成nodejs

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueStrap from 'vue-strap';
+
 Vue.config.debug = true;
 
 import AppComponent from './components/app-component/app-component';
 
 new Vue({
-  el: '#app',
-  components: {
-    'app-component': AppComponent
-  },
-  data: {
-  }
+    el: '#app',
+    components: {
+        'app-component': AppComponent
+    },
+    data: {}
 });
