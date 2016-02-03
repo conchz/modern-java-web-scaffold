f4fd24710f13213e8e52de5990a7541812d4fea9
Baymax <dolphineor@gmail.com>
2016-02-03 21:25:48 +0800

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,28 @@
 module.exports = {
-    entry: './app/main',
+    entry: './app/main.js',
     output: {
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: './dist/'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015']
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+	    {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+	    {
+                test: /\.(jpg|png)$/,
+                loader: 'url?limit=8192'
             }
-        }]
+        ]
     }
 };
