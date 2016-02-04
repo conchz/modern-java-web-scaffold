1312136ae5ec83ce1f959c8083aee009d38a216f
Baymax <dolphineor@gmail.com>
2016-02-04 19:32:27 +0800

    [master] Update main.js

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import VueStrap from 'vue-strap';
 
 Vue.config.debug = true;
+Vue.use(VueResource);
 
 import AppComponent from './components/app-component/app-component';
 
@@ -11,5 +13,15 @@ new Vue({
     components: {
         'app-component': AppComponent
     },
-    data: {}
+    data: {},
+    http: {
+        root: '/api',
+        headers: {
+            Authorization: 'Basic YXBpOnBhc3N3b3Jk'
+        },
+        options: {
+            emulateJSON: true,
+            emulateHTTP: true
+        }
+    }
 });
