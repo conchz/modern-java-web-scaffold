b3fe658962ffbf2a0230002a4679029dfe531598
Baymax <dolphineor@gmail.com>
2016-02-16 18:06:28 +0800

    [master] Update express code with webpack

--- frontend/app.js
+++ frontend/app.js
@@ -2,21 +2,48 @@
 
 "use strict";
 
+let path = require('path');
 let express = require('express');
-let app = express();
+let webpack = require('webpack');
+let webpackMiddleware = require('webpack-dev-middleware');
+let webpackHotMiddleware = require('webpack-hot-middleware');
+let config = require('./webpack.config.js');
 
-const PORT = 8000;
+const isDev = process.env.NODE_ENV !== 'production';
+const port = isDev ? 8000 : process.env.PORT;
+const app = express();
 
-//if (process.env.NODE_ENV === 'production') {
-//    require('./webpack/webpack-prod.server.js')(app)
-//} else {
-//    require('./webpack/webpack-dev.server.js')(app)
-//}
+if (isDev) {
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        contentBase: 'src',
+        stats: {
+            colors: true,
+            hash: false,
+            timings: true,
+            chunks: false,
+            chunkModules: false,
+            modules: false
+        }
+    });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-});
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', function response(req, res) {
+        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
+} else {
+    app.use(express.static(__dirname + '/dist'));
+    app.get('*', function response(req, res) {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+}
 
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:%s', PORT)
+app.listen(port, '0.0.0.0', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,1 +1,105 @@
-module.exports = require('./make-webpack-config')({});
+"use strict";
+
+let path = require('path');
+let webpack = require('webpack');
+let vue = require('vue-loader');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let StatsWebpackPlugin = require('stats-webpack-plugin');
+
+module.exports = {
+    entry: [],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        publicPath: '/assets/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules|vue\/src|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.(jpeg|jpg|png|gif|svg)$/,
+                loader: 'url?limit=8192'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new ExtractTextPlugin('styles.css'),
+        new webpack.NoErrorsPlugin()
+    ],
+    babel: {
+        presets: ["es2015"],
+        plugins: ["transform-runtime"],
+        cacheDirectory: true
+    },
+    vue: {
+        loaders: {
+            css: ExtractTextPlugin.extract("css"),
+            sass: ExtractTextPlugin.extract('css!sass')
+        }
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.entry = (module.exports.entry || []).concat([
+        path.join(__dirname, 'src/main.js')
+    ]);
+
+    // http://vuejs.github.io/vue-loader/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new StatsPlugin('webpack.stats.json', {
+            source: false,
+            modules: false
+        })
+    ])
+} else {
+    module.exports.debug = true;
+    module.exports.devtool = 'eval-source-map';
+    module.exports.entry = (module.exports.entry || []).concat([
+        'webpack-hot-middleware/client?reload=true',
+        path.join(__dirname, 'src/main.js')
+    ]);
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: '"development"'
+            }
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ])
+}
