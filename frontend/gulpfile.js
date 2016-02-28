0e4ea80fc7723929f3be1a92efd1269c68a86c36
Baymax <dolphineor@gmail.com>
2016-02-28 19:50:06 +0800

    Fix Gulp build error

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,52 +1,85 @@
-'use strict';
+"use strict";
 
 const path = require('path'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
     express = require('express'),
-    fallback = require('express-history-api-fallback'),
+    favicon = require('serve-favicon'),
+    rimraf = require('rimraf'),
     webpack = require('webpack'),
-    WebpackDevServer = require('webpack-dev-server'),
+    webpackDevMiddleware = require('webpack-dev-middleware'),
+    webpackHotMiddleware = require('webpack-hot-middleware'),
     devConfig = require('./webpack.dev.config.js'),
-    prodConfig = require('./webpack.prod.config.js');
+    prodConfig = require('./webpack.prod.config.js'),
+    app = express(),
+    port = (process.env.NODE_ENV !== 'production') ? 8000 : process.env.PORT,
+    configFavicon = app => app.use(favicon(path.join(__dirname, 'src/images/favicon.ico'))),
+    startApp = app =>
+        app.listen(port, '0.0.0.0', err => {
+            if (err) throw new gutil.PluginError('dev-server', err);
+            gutil.log('[dev-server]', '==> Listening on port ' + port + '. Open up http://0.0.0.0:' + port + '/ in your browser.')
+        });
 
-gulp.task("default", ["webpack-dev-server"]);
+gulp.task('default', ['dev-server']);
 
-gulp.task("build-dev", ["webpack:build-dev"], () => {
-    gulp.watch(["src/**/*"], ["webpack:build-dev"]);
+gulp.task('clean', cb =>
+    rimraf('dist/*', err => {
+        if (err) throw new gutil.PluginError('clean', err);
+        cb()
+    })
+);
+
+gulp.task('build-dev', ['clean', 'webpack:build-dev'], () => {
+    gulp.watch(['src/**/*'], ['webpack:build-dev']);
 });
 
 // Production build
-gulp.task("build", ["webpack:build"]);
+gulp.task('build', ['clean', 'webpack:build-prod']);
 
-gulp.task("webpack:build", cb => {
-    webpack(Object.create(prodConfig), (err, stats) => {
-        if (err) throw new gutil.PluginError("webpack:build", err);
-        gutil.log("[webpack:build]", stats.toString({
-            colors: true
-        }));
-        cb();
-    });
-});
-
-gulp.task("webpack:build-dev", cb => {
+gulp.task('webpack:build-dev', cb =>
     webpack(Object.create(devConfig)).run((err, stats) => {
-        if (err) throw new gutil.PluginError("webpack:build-dev", err);
-        gutil.log("[webpack:build-dev]", stats.toString({
+        if (err) throw new gutil.PluginError('webpack:build-dev', err);
+        gutil.log('[webpack:build-dev]', stats.toString({
             colors: true
         }));
-    });
-});
 
-gulp.task("webpack-dev-server", cb => {
-    // Start a webpack-dev-server
-    new WebpackDevServer(webpack(Object.create(devConfig)), {
-        publicPath: "/" + devConfig.output.publicPath,
-        stats: {
+        cb()
+    })
+);
+
+gulp.task('webpack:build-prod', cb =>
+    webpack(Object.create(prodConfig), (err, stats) => {
+        if (err) throw new gutil.PluginError('webpack:build-prod', err);
+        gutil.log('[webpack:build-prod]', stats.toString({
             colors: true
+        }));
+
+        cb()
+    })
+);
+
+gulp.task('dev-server', ['build-dev'], () => {
+    const compiler = webpack(devConfig);
+    const devMiddleware = webpackDevMiddleware(compiler, {
+        publicPath: devConfig.output.publicPath,
+        contentBase: 'src',
+        stats: {
+            colors: true,
+            hash: false,
+            timings: true,
+            chunks: false,
+            chunkModules: false,
+            modules: false
         }
-    }).listen(8000, "localhost", err => {
-        if (err) throw new gutil.PluginError("webpack-dev-server", err);
-        gutil.log("[webpack-dev-server]", "http://localhost:8000/webpack-dev-server/index.html");
     });
+
+    app.use(devMiddleware);
+    app.use(webpackHotMiddleware(compiler));
+    configFavicon(app);
+    app.get('*', (req, res) => {
+        res.write(devMiddleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
+
+    startApp(app)
 });
